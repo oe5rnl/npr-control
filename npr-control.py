d99d7dfb72dfa7985cbc5b6b5eb602bcbf653487
oe5rnl <reinhold.autengruber@aon.at>
2021-02-05 14:56:03 +0100

    Update npr-control.py

--- npr-control.py
+++ npr-control.py
@@ -1,5 +1,4 @@
-
-# Copyright (C)
+Copyright (C)
 # 2021-01-30 
 #  Reinhold Autengruber, OE5RNL
 #  Manfred Autengruber,  OE5NVL
@@ -9,10 +8,13 @@
 # V0.1b 2021-01-30
 # - more testing needed
 # - ugly time.sleep
-
+# V0.1c 2021-02-05
+# - like above
+# - ser open changed
 
 import serial
 import time
+import os
 
 # sudo interceptty /dev/ttyACM0 /dev/ttydummy
 
@@ -42,13 +44,42 @@ def validate_qrg(qrg):
 
 class Npr():
 
-  def init(self,com):
-    print(com)
-    self.ser = serial.Serial(com)
-    self.ser.close()
-    self.ser.port = com; 
-    self.ser.baudrate = 921600 
-    self.ser.open()
+  connected = False
+  _com =""
+  retErrComText = "npr: com not available! USB Power down ?"
+
+  def __init__(self, com):
+    self._com = com
+    print(self._com)
+
+    
+  def ser_open(self):
+    #print("COM: "+self._com)
+    if os.path.exists(self._com):
+      self.ser = serial.Serial(self._com)
+      self.ser.close()
+      self.ser.port = self._com; 
+      self.ser.baudrate = 921600 
+      self.ser.open()
+      #print("ser filno "+str(self.ser.fileno()))
+      self.connected = True
+    else:
+      print("ser seropen false")
+      self.connected = False
+    return self.connected
+
+
+  def ser_close():
+     self.close()
+
+
+  def isConnected():
+    self.connected = False
+    if os.path.exists(com):
+      if self.ser.fileno():
+        self.connected = True
+    return self.connected
+
 
   def sendCommand(self,cmd):    
     for element in cmd:
@@ -62,246 +93,311 @@ class Npr():
     while self.ser.inWaiting() > 0:
        s += self.ser.read(1).decode("utf-8")  
        #time.sleep(0.01)
+    self.ser.close()
     return s
 
+
+
   def version(self):
-    self.sendCommand("version")
-    time.sleep(0.5)
-    return self.getResponse()
+    if self.ser_open():
+      self.sendCommand("version")
+      time.sleep(0.5)
+      return self.getResponse()
+    return self.retErrComText
 
   def radio(self,on_off):
-    if on_off=="on":
-      self.sendCommand("radio on")
-    if on_off=="off":
-      self.sendCommand("radio off")
-    time.sleep(1)
-    return self.getResponse()
+    if self.ser_open():
+      if on_off=="on":
+        self.sendCommand("radio on")
+      if on_off=="off":
+        self.sendCommand("radio off")
+      time.sleep(1)
+      return self.getResponse()
+    return self.retErrComText
 
   def status(self):
-    self.sendCommand("status")
-    time.sleep(0.5)
-    self.ser.write(chr(3).encode("utf-8"))
-    r = self.getResponse()
-    return r
+    if self.ser_open():
+      self.sendCommand("status")
+      time.sleep(0.5)
+      self.ser.write(chr(3).encode("utf-8"))
+      r = self.getResponse()
+      return r
+    return self.retErrComText
 
   def who(self):
-    self.sendCommand("who")
-    time.sleep(0.5)
-    self.ser.write(chr(3).encode("utf-8"))
-    return self.getResponse()
+    if self.ser_open():
+      self.sendCommand("who")
+      time.sleep(0.5)
+      self.ser.write(chr(3).encode("utf-8"))
+      return self.getResponse()
+    return self.retErrComText
 
   def display_config(self):
-    self.sendCommand("display config")
-    time.sleep(0)
-    return self.getResponse()
+    if self.ser_open():
+      self.sendCommand("display config")
+      time.sleep(0)
+      return self.getResponse()
+    return self.retErrComText
 
   def display_DHCP_ARP(self):
-    self.sendCommand("display DHCP_ARP")
-    time.sleep(0.5)
-    return self.getResponse()
+    if self.ser_open():
+      self.sendCommand("display DHCP_ARP")
+      time.sleep(0.5)
+      return self.getResponse()
+    return self.retErrComText
 
   def TX_test(self,t):
-    self.sendCommand("TX_test "+str(t))
-    time.sleep(int(t))
-    return self.getResponse()
-    pass
+    if self.ser_open():
+      self.sendCommand("TX_test "+str(t))
+      time.sleep(int(t))
+      return self.getResponse()
+    return self.retErrComText
 
   def set_callsign(self,call):
-    self.sendCommand("set callsign "+call)
-    time.sleep(0.5)
-    return self.getResponse()
+    if self.ser_open():
+      self.sendCommand("set callsign "+call)
+      time.sleep(0.5)
+      return self.getResponse()
+    return self.retErrComText
   
   # testen
   def set_is_master(self,yn):
-    if ((yn=="yes") or (yn=="no")):
-      self.sendCommand("set is_master "+ yn)
-      time.sleep(0.5)
-      return self.getResponse()
-    else:
-      return "ERROR: set is_master: "+yn
+    if self.ser_open():
+      if ((yn=="yes") or (yn=="no")):
+        self.sendCommand("set is_master "+ yn)
+        time.sleep(0.5)
+        return self.getResponse()
+      else:
+        return "ERROR: set is_master: "+yn
+      return self.retErrComText
 
   # offen
   def set_master_FDD(self,m):
-    pass
+    if self.ser_open():
+      return "not defined"
+    return self.retErrComText
 
   # TESTEN !!!
   def set_Eth_mode(self,v):
-    if ((v>=0) and (v<=7)):
-      self.sendCommand("set Eth_mode" + str(v))
-      return self.getResponse()
-    else:
-      return "Error set_Eth_mode: "+str(v)
+    if self.ser_open():
+      if ((v>=0) and (v<=7)):
+        self.sendCommand("set Eth_mode" + str(v))
+        return self.getResponse()
+      else:
+        return "Error set_Eth_mode: "+str(v)
+    return self.retErrComText
 
   def set_modem_IP(self, ip):
-    if validate_ip(ip):
-      self.sendCommand("set modem_IP "+ ip)
-      time.sleep(0.5)
-      self.getResponse()
-    else:
-      return "ERROR ip Adress: "+ ip
+    if self.ser_open():
+      if validate_ip(ip):
+        self.sendCommand("set modem_IP "+ ip)
+        time.sleep(0.5)
+        self.getResponse()
+      else:
+        return "ERROR ip Adress: "+ ip
+    return self.retErrComText
 
   def set_netmask(self,mask):
-    if validate_ip(mask):
-      self.sendCommand("set netmask "+ mask)
-      time.sleep(0.5)
-      self.getResponse()
-    else:
-      return "ERROR Netmask: "+ mask
+    if self.ser_open():
+      if validate_ip(mask):
+        self.sendCommand("set netmask "+ mask)
+        time.sleep(0.5)
+        self.getResponse()
+      else:
+        return "ERROR Netmask: "+ mask
+    return self.retErrComText
 
   def set_telnet_active(self,yn):
-    if ((yn=="yes") or (yn=="no")):
-      self.sendCommand("set telnet_active "+yn)
-      time.sleep(0.5)
-      return self.getResponse()
-    else:
-      return "ERROR set_telnet_active "+yn
+    if self.ser_open():
+      if ((yn=="yes") or (yn=="no")):
+        self.sendCommand("set telnet_active "+yn)
+        time.sleep(0.5)
+        return self.getResponse()
+      else:
+        return "ERROR set_telnet_active "+yn
+    return self.retErrComText
 
   def set_DNS_acitive(self,yn):
-    if ((yn=="yes") or (yn=="no")):
-      self.sendCommand("set DNS_active "+ yn)
-      time.sleep(1)
-      return self.getResponse()
-    else:
-      return "ERROR: set DNS_acitive: "+yn
+    if self.ser_open():
+      if ((yn=="yes") or (yn=="no")):
+        self.sendCommand("set DNS_active "+ yn)
+        time.sleep(1)
+        return self.getResponse()
+      else:
+        return "ERROR: set DNS_active: "+yn
+    return self.retErrComText
 
   def set_DNS_value(self, ip):
-    if validate_ip(ip):
-      self.sendCommand("set DNS_value "+ ip)
-      time.sleep(0.5)
-      self.getResponse()
-    else:
-      return "ERROR ip Adress: "+ ip
+    if self.ser_open():
+      if validate_ip(ip):
+        self.sendCommand("set DNS_value "+ ip)
+        time.sleep(0.5)
+        self.getResponse()
+      else:
+        return "ERROR ip Adress: "+ ip
+    return self.retErrComText
 
   def set_def_route_active(self, yn):
-    if ((yn=="yes") or (yn=="no")):
-      self.sendCommand("set def_route_active "+ yn)
-      time.sleep(1)
-      return self.getResponse()
-    else:
-      return "ERROR: yn: "+yn
+    if self.ser_open():
+      if ((yn=="yes") or (yn=="no")):
+        self.sendCommand("set def_route_active "+ yn)
+        time.sleep(1)
+        return self.getResponse()
+      else:
+        return "ERROR: yn: "+yn
+    return self.retErrComText
 
   def set_def_route_val(self,ip):
-    if validate_ip(ip):
-      self.sendCommand("set def_route_val "+ ip)
-      time.sleep(0.5)
-      self.getResponse()
-    else:
-      return "ERROR ip Adress: "+ ip
+    if self.ser_open():
+      if validate_ip(ip):
+        self.sendCommand("set def_route_val "+ ip)
+        time.sleep(0.5)
+        self.getResponse()
+      else:
+        return "ERROR ip Adress: "+ ip
+    return self.retErrComText
 
   def set_IP_begin(self, ip):
-    if validate_ip(ip):
-      self.sendCommand("set IP_begin "+ ip)
-      time.sleep(0.5)
-      self.getResponse()
-    else:
-      return "ERROR ip Adress: "+ ip
+    if self.ser_open():
+      if validate_ip(ip):
+        self.sendCommand("set IP_begin "+ ip)
+        time.sleep(0.5)
+        self.getResponse()
+      else:
+        return "ERROR ip Adress: "+ ip
+    return self.retErrComText
 
   def set_master_IP_size(self,num):
-    if ((num>0) and (num<=32)):
-      self.sendCommand("set master_IP_size "+ str(num))
-      time.sleep(0.5)
-    else:
-      return "ERROR ip num: " + str(num)
+    if self.ser_open():
+      if ((num>0) and (num<=32)):
+        self.sendCommand("set master_IP_size "+ str(num))
+        time.sleep(0.5)
+      else:
+        return "ERROR ip num: " + str(num)
+    return self.retErrComText
 
   def set_client_req_size(self,num):
-    if ((num>0) and (num<=5)):
-      self.sendCommand("set client_req_size "+ str(num))
-      time.sleep(0.5)
-    else:
-      return "ERROR set_client_req_size num: " + str(num)
+    if self.ser_open():
+      if ((num>0) and (num<=5)):
+        self.sendCommand("set client_req_size "+ str(num))
+        time.sleep(0.5)
+      else:
+        return "ERROR set_client_req_size num: " + str(num)
+    return self.retErrComText
 
   def set_master_IP_down(self, ip):
-    if validate_ip(ip):
-      self.sendCommand("set master_IP_down "+ ip)
-      time.sleep(0.5)
-      self.getResponse()
-    else:
-      return "ERROR ip Adress: "+ ip
+    if self.ser_open():
+      if validate_ip(ip):
+        self.sendCommand("set master_IP_down "+ ip)
+        time.sleep(0.5)
+        self.getResponse()
+      else:
+        return "ERROR ip Adress: "+ ip
+    return self.retErrComText
 
   def set_DHCP_active(self, yn):
-    if ((yn=="yes") or (yn=="no")):
-      self.sendCommand("set DHCP_active "+ yn)
-      time.sleep(1)
-      return self.getResponse()
-    else:
-      return "ERROR: set_DHCP_active: "+yn
+    if self.ser_open():
+      if ((yn=="yes") or (yn=="no")):
+        self.sendCommand("set DHCP_active "+ yn)
+        time.sleep(1)
+        return self.getResponse()
+      else:
+        return "ERROR: set_DHCP_active: "+yn
+    return self.retErrComText
 
   def set_radio_on_at_start(self, yn):
-    if ((yn=="yes") or (yn=="no")):
-      self.sendCommand("set radio_on_at_start "+ yn)
-      time.sleep(1)
-      return self.getResponse()
-    else:
-      return "ERROR: set_radio_on_at_start: "+yn
+    if self.ser_open():
+      if ((yn=="yes") or (yn=="no")):
+        self.sendCommand("set radio_on_at_start "+ yn)
+        time.sleep(1)
+        return self.getResponse()
+      else:
+        return "ERROR: set_radio_on_at_start: "+yn
+    return self.retErrComText
 
   def set_frequency(self, qrg):
-    if validate_qrg(qrg):
-      self.sendCommand("set frequency "+ qrg)
-      time.sleep(3.0)
-      return self.getResponse()
-    else:
-      return "ERROR set frequency: "+ qrg
+    if self.ser_open():
+      if validate_qrg(qrg):
+        self.sendCommand("set frequency "+ qrg)
+        time.sleep(3.0)
+        return self.getResponse()
+      else:
+        return "ERROR set frequency: "+ qrg
+    return self.retErrComText
 
   def set_RF_power(self, p):
-    if ((p>=0) and (p<=127)):
-      self.sendCommand("set RF_power "+ str(p))
-      time.sleep(1)
-      return self.getResponse()
-    else:
-      return "ERROR: set_RF_power: "+str(p)
+    if self.ser_open():
+      if ((p>=0) and (p<=127)):
+        self.sendCommand("set RF_power "+ str(p))
+        time.sleep(1)
+        return self.getResponse()
+      else:
+        return "ERROR: set_RF_power: "+str(p)
+    return self.retErrComText
 
   def set_modulation(self, m):
-    ml = ["11","12","13","14","20","21","22","23","24"]
-    if (m in ml):
-      self.sendCommand("set modulation "+ m)
-      time.sleep(2)
-      return self.getResponse()
-    else:
-      return "ERROR: set_modulation: " + m
+    if self.ser_open():
+      ml = ["11","12","13","14","20","21","22","23","24"]
+      if (m in ml):
+        self.sendCommand("set modulation "+ m)
+        time.sleep(2)
+        return self.getResponse()
+      else:
+        return "ERROR: set_modulation: " + m
+    return self.retErrComText
 
   def set_radio_netw_id(self,v):
-    r =""
-    if ((v>=0) and (v<=15)):
-      self.sendCommand("set radio_netw_ID "+ str(v))
-      time.sleep(2)
-      r = self.getResponse()
-      time.sleep(2)
-      return r + self.getResponse() 
-    else:
-      return "ERROR: set_radio_netw_id: "+str(v)
+    if self.ser_open():
+      r =""
+      if ((v>=0) and (v<=15)):
+        self.sendCommand("set radio_netw_ID "+ str(v))
+        time.sleep(2)
+        r = self.getResponse()
+        time.sleep(2)
+        return r + self.getResponse() 
+      else:
+        return "ERROR: set_radio_netw_id: "+str(v)
+    return self.retErrComText
 
   def save(self):
-    self.sendCommand("save")
-    time.sleep(1)
-    return self.getResponse() 
+    if self.ser_open():
+      self.sendCommand("save")
+      time.sleep(1)
+      return self.getResponse() 
+    return self.retErrComText
 
   def reboot(self):
-    self.sendCommand("reboot")
+    if self.ser_open():
+      self.sendCommand("reboot")
+    return self.retErrComText
 
   def reset_to_default(self):
-    self.sendCommand("reset_to_default")
-    time.sleep(2)
-    return 
+    if self.ser_open():
+      self.sendCommand("reset_to_default")
+      time.sleep(2)
+      return 
+    return self.retErrComText
 
 
   def exit(sef):
-    pass
+    if self.ser_open():
+      pass
+    return self.retErrComText
 
 
 def main():
 
-  npr = Npr()
-  #npr.init("/dev/ttydummy")
-  npr.init("/dev/ttyACM0")
+  npr = Npr("/dev/ttyACM0")
+  #npr = Npr("/dev/ttydummy")
 
-  print(npr.version())
+  #print(npr.version())
   #print(npr.radio("off"))
   #print(npr.status())
   #print(npr.who())
   #print(npr.display_config())
   #print(npr.display_DHCP_ARP())
   #print(npr.TX_test(5))
-  print(npr.set_callsign("OE5RNL"))
+  #print(npr.set_callsign("OE5RNL"))
   #print(npr.set_is_master("no"))
   #print(npr.set_master_FDD(m))
   #print(npr.set_Eth_mode(5))
@@ -326,7 +422,7 @@ def main():
 
   #print(npr.reset_to_default())
 
-  print(npr.display_config())
+  #print(npr.display_config())
 
 if __name__ == "__main__":
   # execute only if run as a script
